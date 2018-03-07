32558f75a192095476cc40709cb12ca97b3134ab
ujiet <t3265199847@gmail.com>
2018-03-08 03:29:28 +0800

    Revised extra blanks

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -21,10 +21,10 @@ entry *append(char lastName[], entry *e)
     /* allocate memory for the new entry and put lastName */
 
     if (((int64_t)e & 0x1f) != 0x00) {
+        /* alignment */
         e->pNext = (entry *) malloc(sizeof(entry)+64);
         void *new_addr = (void *) (((int64_t)&(e->pNext)+64) & ~(int64_t)0x3f);
         void *old_addr = (void *) e->pNext;
-
         free(e->pNext);
         char *padding = (char *) malloc((int64_t)new_addr - (int64_t)old_addr);
 
@@ -32,11 +32,8 @@ entry *append(char lastName[], entry *e)
         e = e->pNext;
         strcpy(e->lastName, lastName);
         e->pNext = NULL;
-
         free(padding);
     }
-
-
 
     else {
         e->pNext = (entry *) malloc(sizeof(entry));
@@ -44,7 +41,6 @@ entry *append(char lastName[], entry *e)
         strcpy(e->lastName, lastName);
         e->pNext = NULL;
     }
-
 
     return e;
 }
